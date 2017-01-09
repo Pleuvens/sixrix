f96d225023d4bcc96759b7bd0eac187501064ef2
Pleuvens Fervil <pleuvens.fervil@epita.fr>
2017-01-09 15:54:57 +0100

    begin hmm

--- HMM/hmm.c
+++ HMM/hmm.c
@@ -1,1 +1,37 @@
+# include <stdlib.h>
+# include <stdio.h>
+
 # include "hmm.h"
+
+int is_p_init_valid(struct automate *aut)
+{
+	double sum = 0;
+	for(int i = 0; i < aut->nb_states; ++i)
+	{
+		sum += aut->states[i].p_init;
+	}
+	if(sum == 1)
+	{
+		return 1;
+	}
+	return 0;
+}
+
+int is_t_valid(struct automate *aut)
+{
+	double sum = 0;
+	for(int i = 0; i < aut->nb_states; ++i)
+	{
+		sum += *((double *)aut->states[i].a->item2);
+	}
+	if(sum == 1)
+	{
+		return 1;
+	}
+	return 0;
+}
+
+int main()
+{
+	return 0;	
+}
--- HMM/hmm.h
+++ HMM/hmm.h
@@ -1,5 +1,21 @@
 # ifndef HMM_H
 # define HMM_H
 
+struct tuple {
+	void *item1;
+	void *item2;
+};
+
+struct state {
+	char *name;
+	double p_init; //Probability of being the initial state
+	struct tuple *a; //Probability of the transition between 2 states 
+	struct tuple *b; //Probavility of containing element k
+};
+
+struct automate {
+	int nb_states;
+	struct state *states;	
+};
 
 #endif
