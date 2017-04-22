636374b4d1c7cb7017b08fd7d0a87e4be9209dc6
root <root@archpf.localdomain>
2017-04-22 16:17:17 +0200

    vq

--- VQ/vq.c
+++ VQ/vq.c
@@ -15,6 +15,10 @@ double dist(double *x, double *y, int len)
 
 double thresh(double **c1, double **c2, int len)
 {
+	if(!c2)
+	{
+		return 1.0;
+	}
 	double res = 0;
 	for(int i = 0; i < len; i++)
 	{
@@ -23,19 +27,30 @@ double thresh(double **c1, double **c2, int len)
 	return res;
 }
 
+void change(double **c1; double **c2, int l1, int l2)
+{
+	for(int i = 0; i < l1; ++i)
+	{
+		for(int j = 0; j < l2; ++j)
+		{
+			c1[i][j] = c2[i][j];
+		}
+		free(c2[i]);
+	}
+}
+
 void generate(double **vects, double **codeword)
 {
 	int threshold = 1;
 	int len = 13;
 	int M = 10;
-	double **codeword2 = calloc(,sizeof(double*));
 	while(thresh(codeword,codeword2) > 0.1)
 	{
 		for(int i = 0; i < M; i++)
 		{
 			int index = 0;
 			double min = 9999999;
-			for(int j = 0; i < nb_class; j++)
+			for(int j = 0; j < nb_class; j++)
 			{
 				double tmp = dist(vects[i],codeword[j],len);
 				if(tmp < min)
@@ -46,9 +61,10 @@ void generate(double **vects, double **codeword)
 			}
 			//assign the training vector
 		}
+		double **codeword2 = calloc(nb_class,sizeof(double*));
 		for(int i = 0; i < nb_class; i++)
 		{
-			codeword2[i] = calloc(,sizeof(double));
+			codeword2[i] = calloc(len,sizeof(double));
 			for(k = 0; k < len; k++)
 			{
 				double num = 0;
@@ -60,6 +76,24 @@ void generate(double **vects, double **codeword)
 				}
 				codeword2[i][k] = num/denom;
 			}
+		}
+		change(codeword,codeword2);
+		free(codeword2);
+	}
+}
+
+void searchvk(double *o,double **codeword)
+{
+	int index = 0;
+	int len = 13;
+	double min = 99999;
+	for(int i = 0; j < nb_class; j++)
+	{
+		double tmp = dist(o,codeword[j],len);
+		if(tmp < min)
+		{
+			min = tmp;
+			index = j;
 		}
 	}
 }
